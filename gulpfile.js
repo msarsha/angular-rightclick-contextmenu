0d10808bdb65dd3b49da8f1b05cd24aafb461f90
Matan SarSha <Sarsha@Matans-MacBook-Pro.local>
2016-07-31 21:28:19 +0300

    gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var templateCache = require('gulp-angular-templatecache');
 var css = require('browserify-css');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var clean = require('gulp-clean');
 
 gulp.task('browserify', function () {
     var b = browserify({
@@ -19,12 +20,18 @@ gulp.task('browserify', function () {
         .pipe(gulp.dest('./dist/'))
 });
 
-gulp.task('default', ['browserify', 'templatecache'], function () {
+gulp.task('build-js', ['browserify', 'templatecache'], function () {
     return gulp
         .src('./dist/*.js')
         .pipe(concat('sarsha.contextmenu.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./dist/'));
+})
+
+gulp.task('default', ['build-js'], function(){
+    return gulp
+        .src(['./dist/bundle.js', './dist/templates.js'])
+        .pipe(clean())
 })
 
 gulp.task('templatecache', function () {
